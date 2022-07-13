3aa31814a527727950211bf54fe7529336540976
BobYin <bobyin72@gmail.com>
2022-07-13 15:14:03 +0800

    Initial Commit

---
+++ MJV5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MJV5
+//
+//  Created by Bob Yin on 2022/7/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
